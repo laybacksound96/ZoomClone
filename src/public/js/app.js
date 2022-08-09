7f6ac8642112e3e1632bb9274eac10a75159c272
412 <85489519+laybacksound96@users.noreply.github.com>
2022-08-09 20:08:25 +0900

    08-09 #0.4 ~#1.9 WebSocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,40 @@
-alert("hu");
+const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("서버와 연결되었습니다.");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버와 연결이 끊어졌습니다.");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -6,7 +8,30 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/g"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-console.log("hellosssdp");
-app.listen(3000);
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const sockets = [];
+
+wss.on("connection", (Socket) => {
+  sockets.push(Socket);
+  Socket["nickname"] = "ㅇㅇ";
+  console.log("클라이언트와 연결되었습니다.");
+  Socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message);
+    switch (parsedMessage.type) {
+      case "new_message":
+        sockets.forEach((element) =>
+          element.send(`${Socket.nickname}: ${parsedMessage.payload}`)
+        );
+        break;
+      case "nickname":
+        Socket["nickname"] = parsedMessage.payload;
+        break;
+    }
+  });
+});
+
+server.listen(3000, () => console.log("alert five ready to burn"));
